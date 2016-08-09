fe138d5d091ce14a61095992dd90ddc9ae7532cd
zuhrasofyan <zuhrasofyan@gmail.com>
2016-08-09 17:01:52 +0700

    Add header and footer container

---
+++ src/app/header.js
@@ -0,0 +1,5 @@
+angular
+  .module('app')
+  .component('gisHeader', {
+    templateUrl: 'app/header.html'
+  });
--- src/app/hello.js
+++ src/app/hello.js
@@ -1,17 +1,17 @@
 angular
 .module('app')
-.component('app', {
+.component('app2', {
 	templateUrl: 'app/hello.html',
 	controller: TechController
 });
 
 function TechController ($scope) {
-	$scope.awesomeThings = [
-	'HTML5 Boilerplate',
-	'AngularJS',
-	'Karma',
-	'SitePoint'
-	];
+// 	$scope.awesomeThings = [
+// 	'HTML5 Boilerplate',
+// 	'AngularJS',
+// 	'Karma',
+// 	'SitePoint'
+// 	];
 
 	
 }
